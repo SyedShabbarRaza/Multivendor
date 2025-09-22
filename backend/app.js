1b168e1e3c82809d2593cef3bf5e555f6683f142
Sayyed Shabbar Raza <syedshabbarraza208@gmail.com>
2025-09-22 11:13:17 -0700

    twelve

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ import cors from 'cors'
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173",credentials:true },));
+app.use(cors({ origin: "https://multivendor-ivory-gamma.vercel.app",credentials:true },));
 app.use(express.urlencoded({extended:true}))  // To access req.body
 app.use(cookieParser()); //To use Cookies
 
